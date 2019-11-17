839053dcfaec5d0db38ec615e0eb45c775c506f9
psychicbologna <alexander.fukui@gmail.com>
2019-11-16 18:03:02 -0600

    fixed db reference

--- src/server.js
+++ src/server.js
@@ -4,29 +4,22 @@ const knex = require('knex'),
   app = require('./app'),
   { PORT, DATABASE_URL } = require('./config');
 
-const db = knex({
-  client: 'pg',
-  connection: DATABASE_URL,
+// Catch-all 404
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.set('db', db);
-
-// Catch-all 404
-// app.use(function (req, res, next) {
-//   const err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // Catch-all Error handler
-// // Add NODE_ENV check to prevent stacktrace leak
-// app.use(function (err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.json({
-//     message: err.message,
-//     error: app.get('env') === 'development' ? err : {}
-//   });
-// });
+// Catch-all Error handler
+// Add NODE_ENV check to prevent stacktrace leak
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
